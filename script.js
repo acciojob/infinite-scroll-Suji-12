581bfe0e6d08d4417c392b4169a0cb092f36e570
Suji-12 <23csec23sujithas@gmail.com>
2025-11-28 22:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+let count = 1;
+for (let i = 0; i < 10; i++) {
+  addItem();
+}
+
+function addItem() {
+  const li = document.createElement("li");
+  li.textContent = "Item " + count;
+  list.appendChild(li);
+  count++;
+}
+
+list.addEventListener("scroll", function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItem();
+    addItem();
+  }
+});
+
+
